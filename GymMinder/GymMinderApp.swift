677d3ab9f90dabf791615cc9f87ec724dc710722
Serik Mussayev <serik1404500@gmail.com>
2025-02-07 21:28:49 +0100

    Initial xcode project commit

---
+++ GymMinder/GymMinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  GymMinderApp.swift
+//  GymMinder
+//
+//  Created by Serik Musaev on 07.02.2025.
+//
+
+import SwiftUI
+
+@main
+struct GymMinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
