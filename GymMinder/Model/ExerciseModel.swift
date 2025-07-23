fa228809ec21a871c6180b5f6ee2197a95c7de2c
Serik Mussayev <serik1404500@gmail.com>
2025-07-23 23:46:09 +0200

    Refactor Exercise models

--- GymMinder/Model/ExerciseModel.swift
+++ GymMinder/Model/ExerciseModel.swift
@@ -8,28 +8,84 @@
 import Foundation
 
 // MARK: - Data Model
-final class Exercise: Identifiable {
-    let id = UUID()
-    let name: String
-    let sets: Int
-    let reps: Int
-    let weight: String
-    let breakTime: Int // In seconds
-    let imageName: String // Exercise image
-    let instructions: [InstructionSection]
+class Exercise: Identifiable, Hashable {
+    enum Equipment: String, Identifiable, CaseIterable {
+        var id: String { rawValue }
+        case none = "None"
+        case freeWeights = "Free Weights"
+        case cables = "Cables"
+        case strengthMachine = "Strength Machine"
+        case multiUseMachine = "Multi-Use Machine"
+        case pullUpBar = "Pull-Up Bar"
+        case parallelBars = "Parallel Bars"
+    }
     
-    init(name: String, sets: Int, reps: Int, weight: String, breakTime: Int, imageName: String, instructions: [InstructionSection]) {
+    enum ExType: String, Identifiable, CaseIterable {
+        var id: String { rawValue }
+        case weights = "Weights"
+        case bodyweight = "Bodyweights"
+        case fullBody = "Full Body"
+        case cardio = "Cardio"
+        case isometric = "Isometric"
+        case plyometric = "Plyometric"
+        case olympic = "Olympic"
+        case stretching = "Stretching"
+        case yoga = "Yoga"
+    }
+    
+    var id: String { return name }
+    var name: String
+    var imageName: String // Exercise image
+    var equipment: Equipment
+    var exType: ExType
+    var instructions: String
+    
+    var sets: Int?
+    var reps: Int?
+    var weight: Float?
+    var breakTime: Float?
+    
+    init(name: String, imageName: String, equipment: Equipment, exType: ExType, sets: Int?, reps: Int?, weight: Float?, breakTime: Float?, instructions: String) {
         self.name = name
+        self.imageName = imageName
+        self.equipment = equipment
+        self.exType = exType
+        self.instructions = instructions
         self.sets = sets
         self.reps = reps
         self.weight = weight
         self.breakTime = breakTime
-        self.imageName = imageName
-        self.instructions = instructions
+    }
+    
+    func copy(src: Exercise) {
+        self.name = src.name
+        self.imageName = src.imageName
+        self.equipment = src.equipment
+        self.exType = src.exType
+        self.instructions = src.instructions
+        self.sets = src.sets
+        self.reps = src.reps
+        self.weight = src.weight
+        self.breakTime = src.breakTime
+    }
+    
+    static func == (lhs: Exercise, rhs: Exercise) -> Bool {
+        return lhs.id == rhs.id
+    }
+    
+    func hash(into hasher: inout Hasher) {
+        hasher.combine(id)
     }
 }
 
-struct InstructionSection {
-    let title: String
-    let steps: [String]
+struct ExerciseOverview {
+    let name: String
+    let imageName: String
+    let equipment: String
+    let exType: String
+    let instructions: String
+    let weight: String
+    let sets: Int
+    let reps: Int
+    let breakTime: Int
 }
